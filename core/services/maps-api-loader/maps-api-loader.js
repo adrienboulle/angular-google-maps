5f4e21b060b55f8f9bdc06c93e0766b8ec0c8ae9
Adrien Boullé <adrien.boulle@gadz.org>
2017-04-30 21:54:37 +0200

    build

---
+++ core/services/maps-api-loader/maps-api-loader.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var MapsAPILoader = (function () {
+    function MapsAPILoader() {
+    }
+    return MapsAPILoader;
+}());
+MapsAPILoader.decorators = [
+    { type: core_1.Injectable },
+];
+/** @nocollapse */
+MapsAPILoader.ctorParameters = function () { return []; };
+exports.MapsAPILoader = MapsAPILoader;
+//# sourceMappingURL=maps-api-loader.js.map
